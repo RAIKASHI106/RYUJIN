cc8d512927c0a6e6e5139b404f545dadc6f30d86
Raikashi <70040264+RAIKASHI106@users.noreply.github.com>
2023-11-29 13:55:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
 #The begining of a new adventure
+
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+
+class TachiyomiHomePage(App):
+    def build(self):
+        layout = GridLayout(cols=1, spacing=10, padding=10)
+        
+        # Header
+        header = Label(text="Tachiyomi", font_size='24sp')
+        layout.add_widget(header)
+        
+        # Sample content - Replace this with content similar to Tachiyomi's homepage
+        content = Label(text="Explore the latest manga releases!")
+        layout.add_widget(content)
+        
+        # Buttons or other widgets
+        button1 = Button(text="Popular Manga")
+        layout.add_widget(button1)
+        
+        button2 = Button(text="New Releases")
+        layout.add_widget(button2)
+        
+        # Add more widgets as needed
+        
+        return layout
+
+
+if __name__ == '__main__':
+    TachiyomiHomePage().run()
